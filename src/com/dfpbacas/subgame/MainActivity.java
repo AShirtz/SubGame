4cd5910f3f31933e9f3a238c86ea4748beb5458e
Andrew Shirtz <anshirtz@nmu.edu>
2013-11-01 10:55:49 -0400

    Added the SubGameLogicLib.jar library to the /libs/ folder and to classpath. Currently running UnitTests.runTests() from a Thread created in the MainActivity.onCreate() method. This is just for testing to prove that the library will work in android.

--- src/com/dfpbacas/subgame/MainActivity.java
+++ src/com/dfpbacas/subgame/MainActivity.java
@@ -1,11 +1,14 @@
 package com.dfpbacas.subgame;
 
 import com.dfpbacas.subgame.MenuFrag;
+import com.example.SubGameObj.Entity.Ship;
+import com.example.SubGameObj.Tests.UnitTests;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -15,7 +18,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				UnitTests.runTests();
+				Log.v("MainActivity", "UnitTests completed");		
+			}
+		}).start();
 	}
 
 	@Override
